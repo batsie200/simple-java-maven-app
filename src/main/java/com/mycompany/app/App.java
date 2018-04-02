3f2c093f761597f45fc598fe4eb250ed9581fe9c
Rubatsiro Chiripa <rubatsiro.chiripa@econet.co.zw>
2018-04-02 08:10:16 +0200

    updated main method again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Rubatsiro Chiripa";
+    private final String message = "Hello World! Rubatsiro Chiripa Mhofu";
 
     public App() {}
 
